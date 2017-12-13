6cc2956cff72e7d8cf25ed5395e06915e598c611
Mouse Reeve <mousereeve@riseup.net>
2017-12-12 16:52:54 -0800

    better example in sample settings

--- bot/settings.sample.py
+++ bot/settings.sample.py
@@ -6,7 +6,7 @@ TWITTER_ACCESS_TOKEN = ''
 TWITTER_ACCESS_SECRET = ''
 
 # the location of your twitter bot's repo, ie:
-# /home/mouse/bestbot
+# /Users/mouse/bestbot
 FILEPATH = ''
 
 # Number of hours between tweets
